23e148e5149f733e2ff958d4c32245bc46fd5a6b
JonathonRyan1 <94103677+JonathonRyan1@users.noreply.github.com>
2021-11-12 02:19:37 +1100

    Update main.c

--- main.c
+++ main.c
@@ -87,6 +87,32 @@ int main(int argc, char *argv[]) {
     insertAndSetFirstWall(&head, 2,  640-120-520, 440, 120, 10);
     */
     
+    // Maze submission on Ed
+    /*
+    insertAndSetFirstWall(&head, 1,  0, 0, 10, OVERALL_WINDOW_HEIGHT);//1
+    insertAndSetFirstWall(&head, 2,  0, 0, OVERALL_WINDOW_WIDTH, 10);//2
+    insertAndSetFirstWall(&head, 3,  70, OVERALL_WINDOW_HEIGHT-10, 570, 10);//3
+    insertAndSetFirstWall(&head, 4,  OVERALL_WINDOW_WIDTH-10, 0, 10, 410);//4
+    //insertAndSetFirstWall(&head, 5,  OVERALL_WINDOW_WIDTH-40, 410, 40, 10);//5
+    insertAndSetFirstWall(&head, 6,  70, 0, 10, 70);//6
+    insertAndSetFirstWall(&head, 7,  70, 140, 10, 340);//7
+    insertAndSetFirstWall(&head, 8,  70, 70, 140, 10);//8
+    insertAndSetFirstWall(&head, 9,  70, 140, 70, 10);//9
+    insertAndSetFirstWall(&head, 10,  140, 140, 10, 70);//10
+    insertAndSetFirstWall(&head, 11,  140, 280, 10, 200);//11
+    insertAndSetFirstWall(&head, 12,  210, 70, 10, 270);//12
+    insertAndSetFirstWall(&head, 13,  210, 410, 80, 10);//13
+    insertAndSetFirstWall(&head, 14,  280, 0, 10, 410);//14
+    insertAndSetFirstWall(&head, 15,  350, 70, 70, 10);//15
+    insertAndSetFirstWall(&head, 16,  350, 70, 10, 410);//16
+    insertAndSetFirstWall(&head, 17,  420, 140, 10, 270);//17
+    insertAndSetFirstWall(&head, 18,  490, 70, 70, 10);//18
+    insertAndSetFirstWall(&head, 19,  420, 140, 140, 10);//19
+    insertAndSetFirstWall(&head, 20,  490, 210, 10, 270);//20
+    insertAndSetFirstWall(&head, 21,  560, 70, 10, 80);//21
+    insertAndSetFirstWall(&head, 22,  490, 210, 70, 10);//22
+    insertAndSetFirstWall(&head, 23,  560, 210, 10, 270);//23
+    */
     
 
     setup_robot(&robot);
