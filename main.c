b6877a17d5fbb4d6aef6558b38bff1d298a61586
JonathonRyan1 <94103677+JonathonRyan1@users.noreply.github.com>
2021-11-11 19:44:09 +1100

    Including the four basic mazes for demo

--- main.c
+++ main.c
@@ -34,29 +34,60 @@ int main(int argc, char *argv[]) {
     // You describe position of top left corner of wall (x, y), then width and height going down/to right
     // Relative positions are used (OVERALL_WINDOW_WIDTH and OVERALL_WINDOW_HEIGHT)
     // But you can use absolute positions. 10 is used as the width, but you can change this.
-    insertAndSetFirstWall(&head, 1,  0, 0, 10, OVERALL_WINDOW_HEIGHT);//1
-    insertAndSetFirstWall(&head, 2,  0, 0, OVERALL_WINDOW_WIDTH, 10);//2
-    insertAndSetFirstWall(&head, 3,  70, OVERALL_WINDOW_HEIGHT-10, 570, 10);//3
-    insertAndSetFirstWall(&head, 4,  OVERALL_WINDOW_WIDTH-10, 0, 10, 410);//4
-    //insertAndSetFirstWall(&head, 5,  OVERALL_WINDOW_WIDTH-40, 410, 40, 10);//5
-    insertAndSetFirstWall(&head, 6,  70, 0, 10, 70);//6
-    insertAndSetFirstWall(&head, 7,  70, 140, 10, 340);//7
-    insertAndSetFirstWall(&head, 8,  70, 70, 140, 10);//8
-    insertAndSetFirstWall(&head, 9,  70, 140, 70, 10);//9
-    insertAndSetFirstWall(&head, 10,  140, 140, 10, 70);//10
-    insertAndSetFirstWall(&head, 11,  140, 280, 10, 200);//11
-    insertAndSetFirstWall(&head, 12,  210, 70, 10, 270);//12
-    insertAndSetFirstWall(&head, 13,  210, 410, 80, 10);//13
-    insertAndSetFirstWall(&head, 14,  280, 0, 10, 410);//14
-    insertAndSetFirstWall(&head, 15,  350, 70, 70, 10);//15
-    insertAndSetFirstWall(&head, 16,  350, 70, 10, 410);//16
-    insertAndSetFirstWall(&head, 17,  420, 140, 10, 270);//17
-    insertAndSetFirstWall(&head, 18,  490, 70, 70, 10);//18
-    insertAndSetFirstWall(&head, 19,  420, 140, 140, 10);//19
-    insertAndSetFirstWall(&head, 20,  490, 210, 10, 270);//20
-    insertAndSetFirstWall(&head, 21,  560, 70, 10, 80);//21
-    insertAndSetFirstWall(&head, 22,  490, 210, 70, 10);//22
-    insertAndSetFirstWall(&head, 23,  560, 210, 10, 270);//23
+    
+    // BASIC MAZE 1 AND 2
+    
+    insertAndSetFirstWall(&head, 2,  220, 400, 10, 80);
+    insertAndSetFirstWall(&head, 2,  20, 400, 200, 10);
+    insertAndSetFirstWall(&head, 2,  20, 50, 10, 350);
+    insertAndSetFirstWall(&head, 2,  20, 50, 280, 10);
+    insertAndSetFirstWall(&head, 2,  300, 50, 10, 100);
+    insertAndSetFirstWall(&head, 2,  300, 150, 110, 10);
+    insertAndSetFirstWall(&head, 2,  400, 50, 10, 100);
+    insertAndSetFirstWall(&head, 2,  400, 50, 220, 10);
+    insertAndSetFirstWall(&head, 2,  620, 50, 10, 290);
+    insertAndSetFirstWall(&head, 2,  620, 340, 20, 10);
+
+    insertAndSetFirstWall(&head, 1,  320, 300, 10, 180);
+    insertAndSetFirstWall(&head, 2,  120, 300, 200, 10);
+    insertAndSetFirstWall(&head, 2,  120, 150, 10, 150);
+    insertAndSetFirstWall(&head, 2,  120, 150, 80, 10);
+    insertAndSetFirstWall(&head, 2,  200, 150, 10, 100);
+    insertAndSetFirstWall(&head, 2,  200, 250, 310, 10);
+    insertAndSetFirstWall(&head, 2,  500, 150, 10, 100);
+    insertAndSetFirstWall(&head, 2,  500, 150, 10, 100);
+    insertAndSetFirstWall(&head, 2,  500, 150, 20, 10);
+    insertAndSetFirstWall(&head, 2,  520, 150, 10, 290);
+    insertAndSetFirstWall(&head, 2,  520, 440, 120, 10);
+    
+    
+    // BASIC MAZE 3 AND 4
+    /*
+    insertAndSetFirstWall(&head, 2,  640-10-220, 400, 10, 80);
+    insertAndSetFirstWall(&head, 2,  640-200-20, 400, 200, 10);
+    insertAndSetFirstWall(&head, 2,  640-10-20, 50, 10, 350);
+    insertAndSetFirstWall(&head, 2,  640-280-20, 50, 280, 10);
+    insertAndSetFirstWall(&head, 2,  640-10-300, 50, 10, 100);
+    insertAndSetFirstWall(&head, 2,  640-110-300, 150, 110, 10);
+    insertAndSetFirstWall(&head, 2,  640-10-400, 50, 10, 100);
+    insertAndSetFirstWall(&head, 2,  640-400-220, 50, 220, 10);
+    insertAndSetFirstWall(&head, 2,  640-10-620, 50, 10, 290);
+    insertAndSetFirstWall(&head, 2,  640-620-20, 340, 20, 10);
+
+
+    insertAndSetFirstWall(&head, 1,  640-10-320, 300, 10, 180);
+    insertAndSetFirstWall(&head, 2,  640-200-120, 300, 200, 10);
+    insertAndSetFirstWall(&head, 2,  640-10-120, 150, 10, 150);
+    insertAndSetFirstWall(&head, 2,  640-80-120, 150, 80, 10);
+    insertAndSetFirstWall(&head, 2,  640-10-200, 150, 10, 100);
+    insertAndSetFirstWall(&head, 2,  640-310-200, 250, 310, 10);
+    insertAndSetFirstWall(&head, 2,  640-10-500, 150, 10, 100);
+    insertAndSetFirstWall(&head, 2,  640-20-500, 150, 20, 10);
+    insertAndSetFirstWall(&head, 2,  640-10-520, 150, 10, 290);
+    insertAndSetFirstWall(&head, 2,  640-120-520, 440, 120, 10);
+    */
+    
+    
 
     setup_robot(&robot);
     updateAllWalls(head, renderer);
@@ -72,11 +103,20 @@ int main(int argc, char *argv[]) {
         robotMotorMove(&robot);
 
         //Check if robot reaches endpoint. and check sensor values
-        if (checkRobotReachedEnd(&robot, OVERALL_WINDOW_WIDTH-20, OVERALL_WINDOW_HEIGHT-35, 10, 100)){
+        
+        // MAZE 1
+        if (checkRobotReachedEnd(&robot, 640, 340, 10, 100)){ //Maze 1
+        //if (checkRobotReachedEnd(&robot, 220, 480, 100, 10)){ //Maze 2
+        //if (checkRobotReachedEnd(&robot, 0, 340, 10, 100)){ // Maze 3
+        //if (checkRobotReachedEnd(&robot, 640-10-320, 480, 100, 10)){ //Maze 4
             end_time = clock();
             msec = (end_time-start_time) * 1000 / CLOCKS_PER_SEC;
             robotSuccess(&robot, msec);
         }
+        
+        
+        
+        
         else if(checkRobotHitWalls(&robot, head))
             robotCrash(&robot);
         //Otherwise compute sensor information
